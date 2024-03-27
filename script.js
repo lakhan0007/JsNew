56cd9b2af720606df087719217f83b8547103b4f
lakhan0007 <lakhansinghrathore2001@gmail.com>
2024-03-27 17:10:47 +0530

    function

--- script.js
+++ script.js
@@ -29,6 +29,16 @@
 // // console.log("After swapping:" + " x = " + x + ", y = " + y);
 // console.log(`After Swapping: x = ${x} y = ${y}`);
 
+
+
+
+
+
+
+
+
+
+
 // let leepYear;
 
 // leepYear = parseInt(prompt('enter a year'))
@@ -49,10 +59,48 @@
 
 
 
+// let year;
+
+// year = parseInt(prompt('Enter a year'))
+
+// if(year % 4 === 0){
+//   if ( year % 100 !== 0) {
+//     document.write('This is a leap year');
+//   }else{
+//     if( year % 400 === 0){
+//       document.write('This is a leap year');
+//     }else{
+//       document.write('This is not a leap year');
+//     }
+//   }
+// }else{
+//   document.write('This is not a leap year');
+// }
 
 
-let table = parseInt(prompt('Enter Table Number'))
 
-for(let value = 1; value <= 10; value++){
-  document.write(` <br/> The table of ${table} * ${value} = ${value * table} <br/> `)
+// if(( year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+//   document.write('This is a leap year')
+// }else{
+//     document.write('This is not a leap year');
+// }
+
+
+
+// let table = parseInt(prompt('Enter Table Number'))
+
+// for(let value = 1; value <= 10; value++){
+//   document.write(` <br/> The table of ${table} * ${value} = ${value * table} <br/> `)
+// }
+
+
+
+let myName = 'lakhan Singh Rathorer'
+
+function himanshi() {
+    let myAge = 28 
+    // console.log(myName , myAge);
 }
+
+himanshi()
+console.log( myName );
